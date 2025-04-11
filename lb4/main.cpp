e6c0e7fd3b298479f8e20a64d04cae8004365cba
kylichist <pimenovpetya@gmail.com>
2025-04-11 18:10:11 +0300

    Исправления ЛР4

--- lb4/main.cpp
+++ lb4/main.cpp
@@ -11,18 +11,21 @@ vector<int> prefix_function(const string& pattern) {
     int len = 0;
     for (int i = 1; i < m;) {
         cout << "i: " << i << ", len: " << len << endl;
+        cout << "Comparing " << pattern[i] << " and " << pattern[len] << endl;
         if (pattern[i] == pattern[len]) {
             cout << "Symbols match" << endl;
             lps[i++] = ++len;
-            cout << "lps[i] = " << len << endl;
+            cout << "lps[" << i - 1 << "] = " << len << endl;
+            cout << "Increase i, i = " << i << endl;
         } else {
             cout << "Symbols do not match" << endl;
             if (len) {
                 cout << "len = " << lps[len - 1] << endl;
                 len = lps[len - 1];
             } else {
-                cout << "lps[i] = 0" << endl;
+                cout << "lps[" << i << "] = 0" << endl;
                 lps[i++] = 0;
+                cout << "Increase i, i = " << i << endl;
             }
         }
         cout << endl;
@@ -50,25 +53,25 @@ vector<int> find(const string& text, const string& pattern) {
     int i = 0, j = 0;
     while (i < n) {
         cout << "i: " << i << ", j: " << j << endl;
+        cout << "Comparing " << text[i] << " and " << pattern[j] << endl;
         if (text[i] == pattern[j]) {
-            cout << "text[i] matches pattern[j]" << endl;
+            cout << "Symbols match" << endl;
             i++, j++;
-            cout << "i++, j++" << endl;
+            cout << "Increase i and j, i = " << i << ", j = " << j << endl;
             if (j == m) {
-                cout << "Substring found at: " << i - j << endl;
+                cout << "Substring found at " << i - j << endl;
                 result.push_back(i - j);
+                cout << "Reset j, j = " << lps[j - 1] << endl;
                 j = lps[j - 1];
             }
         } else {
-            cout << "text[i] does not match pattern[j]" << endl;
+            cout << "Symbols do not match" << endl;
             if (j) {
-                cout << "Reset j" << endl;
-                cout << "j = " << lps[j - 1] << endl;
+                cout << "Reset j, j = " << lps[j - 1] << endl;
                 j = lps[j - 1];
-
             } else {
-                cout << "i += 1" << endl;
                 i++;
+                cout << "j = 0, Increase i, i = " << i << endl;
             }
         }
         cout << endl;
@@ -80,28 +83,28 @@ int main() {
     string pattern, text;
     cin >> pattern >> text;
 
-    // vector<int> matches = find(text, pattern);
-    // if (matches.empty()) {
-    //     cout << "-1" << endl;
-    // } else {
-    //     for (size_t i = 0; i < matches.size(); i++) {
-    //         if (i > 0)
-    //             cout << ",";
-    //         cout << matches[i];
-    //     }
-    //     cout << endl;
+    vector<int> matches = find(text, pattern);
+    if (matches.empty()) {
+        cout << "-1" << endl;
+    } else {
+        for (size_t i = 0; i < matches.size(); i++) {
+            if (i > 0)
+                cout << ",";
+            cout << matches[i];
+        }
+        cout << endl;
+    }
+
+    // if (text.size() != pattern.size()) {
+    //     cout << -1 << endl;
+    //     return 0;
     // }
 
-    if (text.size() != pattern.size()) {
-        cout << -1 << endl;
-        return 0;
-    }
-
-    vector<int> matches = find(pattern + pattern, text);
-    if (matches.empty()) {
-        cout << -1 << endl;
-        return 0;
-    }
-    cout << matches[0] << endl;
+    // vector<int> matches = find(pattern + pattern, text);
+    // if (matches.empty()) {
+    //     cout << -1 << endl;
+    //     return 0;
+    // }
+    // cout << matches[0] << endl;
     return 0;
 }
